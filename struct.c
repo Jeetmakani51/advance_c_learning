4411ee65f6ec51181b0197c9104f52325a858b4c
Jeet <makanijeet51@gmail.com>
2026-06-23 11:40:22 +0530

    binary file format parser

---
+++ struct.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+#pragma pack(1)// no padding between bytes, controls padding and alignment
+
+typedef struct{
+    char signature[2];
+    unsigned int file_size; // file size are never negative so unsigned int which gives full positive range
+    short reserved;
+    short reserverd;
+    unsigned int pixel_data_offset;
+}BMPFILEHEADER;
+
+typedef struct{
+    unsigned int header_size;
+    unsigned int image_width;
+    unsigned int image_height;
+    short color_planes;
+    short bits_per_pixel;
+}BMPDIBHEADER;
+
+#pragma pack(0) // restore default padding after
+
+int main(){
+    BMPFILEHEADER bfh;
+    BMPDIBHEADER bdh;
+
+    FILE *fp;
+    //read
+    fp = fopen("c-code.bmp","rb");
+    if(fp == NULL){
+        printf("cannot open file.\n");
+        return 1;
+    }
+
+    fread(&bfh,sizeof(BMPFILEHEADER), 1, fp);
+    fread(&bdh,sizeof(BMPDIBHEADER), 1, fp);
+    fclose(fp);
+
+    printf("width : %d\n", bdh.image_width);
+    printf("height : %d\n", bdh.image_height);
+    printf("file size : %d\n", bfh.file_size);
+    printf("bits per pixel : %d\n", bdh.bits_per_pixel);
+    return 0;
+}
