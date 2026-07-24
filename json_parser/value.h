e37cb40e419611cd9e1c7e15bcb1ea20eb7e288a
Jeet <makanijeet51@gmail.com>
2026-07-24 18:01:12 +0530

    started building ast/value structs and parser for json

---
+++ json_parser/value.h
@@ -0,0 +1,44 @@
+#ifndef VALUE_H
+#define VALUE_H
+#include "lexer.h"
+
+typedef enum{
+    JSON_STRING,
+    JSON_NUMBER,
+    JSON_BOOL,
+    JSON_NULL,
+    JSON_ARRAY,
+    JSON_OBJECT
+}JsonType;
+
+typedef struct JsonValue{
+    JsonType type;
+    union{
+        char *string;
+        double number;
+        int boolean;
+        struct{
+            struct JsonValue **items;
+            int count;
+            int capacity;
+        }array;
+        struct{
+            char **keys;
+            struct JsonValue **values;
+            int count;
+            int capacity;
+        }object;
+    }value;
+}JsonValue;
+
+JsonValue *make_string(char *s);
+JsonValue *make_number(double n);
+JsonValue *make_bool(int b);
+JsonValue *make_null();
+JsonValue *make_array();
+JsonValue *make_object();
+void array_push(JsonValue *arr, JsonValue *item);
+void object_push(JsonValue *obj, char *key, JsonValue *val);
+void free_value(JsonValue *v);
+
+#endif
