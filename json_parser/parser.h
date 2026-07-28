a1968c3442314f227c344ef77d424289d7bdb57f
Jeet <makanijeet51@gmail.com>
2026-07-28 21:18:09 +0530

    started building parser for json

---
+++ json_parser/parser.h
@@ -0,0 +1,15 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "lexer.h"
+#include "value.h"
+
+typedef struct{
+    Lexer lexer;
+    Token current;
+}Parser;
+
+Parser create_parser(char *source);
+JsonValue *parse_value(Parser *p);
+
+#endif
